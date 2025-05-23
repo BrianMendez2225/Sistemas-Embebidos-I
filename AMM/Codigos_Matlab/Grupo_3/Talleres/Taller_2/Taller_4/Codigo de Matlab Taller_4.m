fedec754c996ee1e48be81d77d0493205f67adb9
Brian Mendez <bmendez4@cuc.edu.co>
2025-05-23 17:20:31 -0500

    Update Codigo de Matlab Taller_4.m

--- AMM/Codigos_Matlab/Grupo_3/Talleres/Taller_2/Taller_4/Codigo de Matlab Taller_4.m
+++ AMM/Codigos_Matlab/Grupo_3/Talleres/Taller_2/Taller_4/Codigo de Matlab Taller_4.m
@@ -1,1 +1,20 @@
+clc
+den = [1 9 30 40];  % s^3 + 9 s^2 + 30 s + 40
+num = 50;
+G= tf(num,den)
 
+omega = 29;
+s = 1j * omega;
+Gjw = num / polyval(den, s);
+magnitude = abs(Gjw);
+phase_deg = angle(Gjw) * (180/pi);
+fprintf('Magnitud en ω = %.2f rad/s: %.6f\n', omega, magnitude);
+fprintf('Fase en ω = %.2f rad/s: %.2f grados\n', omega, phase_deg);
+
+figure;bode(G);
+grid on;
+title('Diagrama de Bode de G(s)');
+
+figure;step(G)
+grid on;
+title('Entrada STEP');
