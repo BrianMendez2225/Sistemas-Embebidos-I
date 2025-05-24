56734171de49414b7ca4622f0c209d2805ab6883
Brian Mendez <bmendez4@cuc.edu.co>
2025-05-23 19:49:11 -0500

    Create Punto_3.m

---
+++ AMM/Codigos_Matlab/Grupo_3/Talleres/Taller_5/Punto_3.m
@@ -0,0 +1,25 @@
+EJERCICIO 3
+
+% Función de transferencia del horno
+G = tf([1], [1 9 4 7]);
+% Controlador PID inicial
+Kp = 10;
+Ki = 12;
+Kd = 20;
+PID = pid(Kp, Ki, Kd);
+% Sistema en lazo cerrado
+T = feedback(PID * G, 1);
+t = 2;
+
+% Simulación de la respuesta al escalón
+subplot(2, 1, 1);  % 2 filas, 1 columna, gráfica 1
+step(G, 10);
+title('Ejercicio 3: Respuesta al escalón del sistema sin controlador');
+grid on;
+
+subplot(2, 1, 2);  % 2 filas, 1 columna, gráfica 2
+step(T, 3);
+grid on;
+title('Ejercicio 3: Respuesta al escalón del sistema con PID');
+xlabel('Tiempo (s)');
+ylabel('Amplitud');
